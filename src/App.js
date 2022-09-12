26abc84ba85016d84f31033a17eae171a805829c
Praise Okhae <praiseomozoba@gmail.com>
2022-09-12 01:31:09 +0100

    commit message

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './Component/Head/Header';
 import Home from './Component/Hero/Home';
 import Features from './Component/Features/Features';
+import Portfolio from './Component/Portfolio/Portfolio';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
      <Header/>
      <Home/>
      <Features/>
+     <Portfolio/>
     </div>
   );
 }
